ab08ecfaa2d9be9d8533aca8f6a7f8843b6dd2ec
Mary Monchari Gichana <marygichana95@gmail.com>
2024-12-21 14:46:20 +0300

    Add Meal class with attributes for name, date, and category ID for initializing a meal instance

--- models/meal.py
+++ models/meal.py
@@ -1,3 +1,10 @@
 import sqlite3
 from app import DATABASE_NAME
 from datetime import datetime
+
+class Meal:
+    def __init__(self, name,date, mealcategory_id, id= None):
+        self.id = id  
+        self.name = name
+        self.date = date  
+        self.mealcategoryid = mealcategory_id

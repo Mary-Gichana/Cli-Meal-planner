fee86ad575239d5ee4b82522feec169629f407aa
Mary Monchari Gichana <marygichana95@gmail.com>
2024-12-21 15:36:12 +0300

    Create mealcategory table in the database if it doesn't exist with two columns id and name

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import sqlite3
 
 DATABASE_NAME = 'database.db'
+
+def create_database():
+    with sqlite3.connect(DATABASE_NAME) as conn:
+        cursor = conn.cursor()
+        
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS mealcategory (
+                id INTEGER PRIMARY KEY ,
+                name TEXT UNIQUE NOT NULL
+            )
+        ''')

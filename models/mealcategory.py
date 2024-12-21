e533c2b1cc5e7ea00ec9c4595f59e17f6b5b539e
Mary Monchari Gichana <marygichana95@gmail.com>
2024-12-21 15:12:20 +0300

    Add setter for name attribute with validation in MealCategory class

--- models/mealcategory.py
+++ models/mealcategory.py
@@ -18,3 +18,9 @@ class MealCategory:
     @property
     def name(self):
         return self._name
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str):
+            self._name = new_name
+        else:
+            raise ValueError("Name must be a string")
